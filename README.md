d06090590ccfc56916f020a78237a605ce1f7ef3
Panos <pansysk75@gmail.com>
2024-11-12 17:27:48 -0600

    Get a basic Lemon example running

---
+++ README.md
@@ -0,0 +1,2 @@
+Lemon Documentation (Internet Archive):
+https://web.archive.org/web/20240304014429/https://lemon.cs.elte.hu/trac/lemon/wiki/Documentation
--- cmake/SetupLemon.cmake
+++ cmake/SetupLemon.cmake
@@ -6,13 +6,16 @@ if(FETCH_LEMON AND NOT TARGET lemon)
   include(FetchContent)
   FetchContent_Declare(
     Lemon
-    GIT_REPOSITORY
-    https://github.com/The-OpenROAD-Project/lemon-graph.git
-    GIT_TAG
-    master
-    )
-    FetchContent_MakeAvailable(Lemon)
+    GIT_REPOSITORY https://github.com/The-OpenROAD-Project/lemon-graph.git
+    GIT_TAG master)
+  FetchContent_MakeAvailable(Lemon)
+
+  # Include directories are not included in the Lemon target, so we need to add
+  # them manually
+  target_include_directories(lemon INTERFACE ${lemon_SOURCE_DIR}/)
+  target_include_directories(lemon INTERFACE ${lemon_BINARY_DIR}/)
 
 elseif(NOT TARGET lemon)
-    find_package(Lemon REQUIRED)
+  # I haven't tested this
+  find_package(Lemon REQUIRED)
 endif()
