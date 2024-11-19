b8d773202830e244959f37fb0bf218f2fcdb35f3
Shreyas Atre <shreyasatre16@gmail.com>
2024-11-18 19:30:22 -0600

    Add clang-format for sanity of formatting
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <lemon/lgf_reader.h>
 #include <lemon/list_graph.h>
 #include <lemon/preflow.h>
-#include <lemon/lgf_reader.h>
 #include <stack>
 #include "mtx_reader.hpp"
 
@@ -19,19 +19,23 @@ class k_min_cut
     ListDigraph::NodeMap<ListDigraph::Node> _p;
     // The min flow map
     ListDigraph::NodeMap<int> _fl;
-    
+
     // The Gomory-Hu Tree
     ListDigraph _tree;
 
 public:
-k_min_cut(ListDigraph const& graph, ListDigraph::ArcMap<int> const& weights)
-    : _graph(graph), _weights(weights), _p(graph), _fl(graph), _tree()
-{
-}
+    k_min_cut(ListDigraph const& graph, ListDigraph::ArcMap<int> const& weights)
+      : _graph(graph)
+      , _weights(weights)
+      , _p(graph)
+      , _fl(graph)
+      , _tree()
+    {
+    }
 
-    
-void run_gomory_hu(){
-    /*
+    void run_gomory_hu()
+    {
+        /*
     for s:=2 to n do
         begin
             Compute a minimum cut between nodes
@@ -49,174 +53,192 @@ void run_gomory_hu(){
         end;
     end;
 */
-    // Choose a root node
-    ListDigraph::NodeIt s(_graph);
+        // Choose a root node
+        ListDigraph::NodeIt s(_graph);
 
-    for (ListDigraph::NodeIt t(s); t != INVALID; ++t){
-        if (s == t) continue;
-        Preflow<ListDigraph, ListDigraph::ArcMap<int>> min_cut(_graph, _weights, s, t);
-        min_cut.run();
-        _fl[t] = min_cut.flowValue();
-        for (ListDigraph::NodeIt i(_graph); i != INVALID; ++i){
-            if (i != s && min_cut.minCut(i) && _p[i] == t){
-                _p[i] = s;
+        for (ListDigraph::NodeIt t(s); t != INVALID; ++t)
+        {
+            if (s == t)
+                continue;
+            Preflow<ListDigraph, ListDigraph::ArcMap<int>> min_cut(
+                _graph, _weights, s, t);
+            min_cut.run();
+            _fl[t] = min_cut.flowValue();
+            for (ListDigraph::NodeIt i(_graph); i != INVALID; ++i)
+            {
+                if (i != s && min_cut.minCut(i) && _p[i] == t)
+                {
+                    _p[i] = s;
+                }
+            }
+            if (min_cut.minCut(_p[t]))
+            {
+                _p[s] = _p[t];
+                _p[t] = s;
+                _fl[s] = _fl[t];
+                _fl[t] = min_cut.flowValue();
             }
         }
-        if (min_cut.minCut(_p[t])){
-            _p[s] = _p[t];
-            _p[t] = s;
-            _fl[s] = _fl[t];
-            _fl[t] = min_cut.flowValue();
+
+        // Create the Gomory-Hu tree. The tree is an undirected graph with the same nodes as the original graph
+        // and edges (i, p[i]) with weight fl[i].
+        // Making it into a graph to make it easier to traverse, however in principle _p is enough to represent the tree.
+        // An important property here is that each node has a single parent (except the root node which has none).
+        for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n)
+        {
+            _tree.addNode();
+        }
+
+        for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n)
+        {
+            if (_p[n] != INVALID)
+            {
+                ListDigraph::Arc a = _tree.addArc(n, _p[n]);
+            }
         }
     }
 
-    // Create the Gomory-Hu tree. The tree is an undirected graph with the same nodes as the original graph
-    // and edges (i, p[i]) with weight fl[i].
-    // Making it into a graph to make it easier to traverse, however in principle _p is enough to represent the tree.
-    // An important property here is that each node has a single parent (except the root node which has none).
-    for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n){
-        _tree.addNode();
-    }
+    int min_k_cut_value(unsigned int k)
+    {
+        // Sum the k smallest values in _fl
+        std::vector<int> k_heap;
 
-    for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n){
-        if (_p[n] != INVALID){
-            ListDigraph::Arc a = _tree.addArc(n, _p[n]);
+        for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n)
+        {
+            k_heap.push_back(_fl[n]);
+            std::push_heap(k_heap.begin(), k_heap.end());
+            if (k_heap.size() > k)
+            {
+                std::pop_heap(k_heap.begin(), k_heap.end());
+                k_heap.pop_back();
+            }
         }
-    }
-}
-
-int min_k_cut_value(unsigned int k){
-    // Sum the k smallest values in _fl
-    std::vector<int> k_heap;
-
-    for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n){
-        k_heap.push_back(_fl[n]);
-        std::push_heap(k_heap.begin(), k_heap.end());
-        if (k_heap.size() > k){
-            std::pop_heap(k_heap.begin(), k_heap.end());
-            k_heap.pop_back();
+        int sum = 0;
+        for (int i = 0; i < k; ++i)
+        {
+            sum += k_heap[i];
         }
+        return sum;
     }
-    int sum = 0;
-    for (int i = 0; i < k; ++i){
-        sum += k_heap[i];
-    }
-    return sum;
-}
 
-void min_k_cut_map(unsigned int k, ListDigraph::NodeMap<unsigned int>& cut_map){
-    // Creates a cut_map, which stores a unique integer value for each connected component
-    // created by the min-k cut
+    void min_k_cut_map(
+        unsigned int k, ListDigraph::NodeMap<unsigned int>& cut_map)
+    {
+        // Creates a cut_map, which stores a unique integer value for each connected component
+        // created by the min-k cut
 
-    // Algorithm: Find ids of the k smallest values in _fl
-    // Make a copy of _tree and delete the k corresponding edges
-    // Color the connected components of the resulting graph
+        // Algorithm: Find ids of the k smallest values in _fl
+        // Make a copy of _tree and delete the k corresponding edges
+        // Color the connected components of the resulting graph
 
-    // Find the k smallest values in _fl
-    std::vector<ListDigraph::Node> k_heap;
-    auto comp = [&](ListDigraph::Node a, ListDigraph::Node b){
-        return _fl[a] < _fl[b];
-    };
-    for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n){
-        k_heap.push_back(n);
-        std::push_heap(k_heap.begin(), k_heap.end(), comp);
-        if (k_heap.size() > k){
-            std::pop_heap(k_heap.begin(), k_heap.end(), comp);
-            k_heap.pop_back();
+        // Find the k smallest values in _fl
+        std::vector<ListDigraph::Node> k_heap;
+        auto comp = [&](ListDigraph::Node a, ListDigraph::Node b) {
+            return _fl[a] < _fl[b];
+        };
+        for (ListDigraph::NodeIt n(_graph); n != INVALID; ++n)
+        {
+            k_heap.push_back(n);
+            std::push_heap(k_heap.begin(), k_heap.end(), comp);
+            if (k_heap.size() > k)
+            {
+                std::pop_heap(k_heap.begin(), k_heap.end(), comp);
+                k_heap.pop_back();
+            }
         }
-    }
 
-    // Make a copy of _tree
-    ListDigraph tree_copy;
-    DigraphCopy<ListDigraph, ListDigraph> copy(_tree, tree_copy);
-    copy.run();
+        // Make a copy of _tree
+        ListDigraph tree_copy;
+        DigraphCopy<ListDigraph, ListDigraph> copy(_tree, tree_copy);
+        copy.run();
 
-    // Delete the k corresponding edges
-    for(auto n : k_heap){
-        // By construction, node n only has a single outgoing edge
-        ListDigraph::OutArcIt e(_tree, n);
-        if (e != INVALID)
-            tree_copy.erase(e);
-    }
+        // Delete the k corresponding edges
+        for (auto n : k_heap)
+        {
+            // By construction, node n only has a single outgoing edge
+            ListDigraph::OutArcIt e(_tree, n);
+            if (e != INVALID)
+                tree_copy.erase(e);
+        }
 
-    // Color the connected components of the resulting graph
-    unsigned int color = 0;
-    // Do a DFS on the tree_copy
-    for (ListDigraph::NodeIt n(tree_copy); n != INVALID; ++n){
-        if (cut_map[n] == 0){
-            color++;
-            std::stack<ListDigraph::Node> stack;
-            stack.push(n);
-            while (!stack.empty()){
-                ListDigraph::Node m = stack.top();
-                stack.pop();
-                cut_map[m] = color;
-                for (ListDigraph::OutArcIt e(tree_copy, m); e != INVALID; ++e){
-                    ListDigraph::Node v = tree_copy.target(e);
-                    if (cut_map[v] == 0){
-                        stack.push(v);
+        // Color the connected components of the resulting graph
+        unsigned int color = 0;
+        // Do a DFS on the tree_copy
+        for (ListDigraph::NodeIt n(tree_copy); n != INVALID; ++n)
+        {
+            if (cut_map[n] == 0)
+            {
+                color++;
+                std::stack<ListDigraph::Node> stack;
+                stack.push(n);
+                while (!stack.empty())
+                {
+                    ListDigraph::Node m = stack.top();
+                    stack.pop();
+                    cut_map[m] = color;
+                    for (ListDigraph::OutArcIt e(tree_copy, m); e != INVALID;
+                        ++e)
+                    {
+                        ListDigraph::Node v = tree_copy.target(e);
+                        if (cut_map[v] == 0)
+                        {
+                            stack.push(v);
+                        }
                     }
                 }
             }
         }
     }
-}
-
 };
 
+void test_graph_read()
+{
+    char test_lgf_graph[] = "@nodes\n"
+                            "label\n"
+                            "0\n"
+                            "1\n"
+                            "2\n"
+                            "3\n"
+                            "4\n"
+                            "@arcs\n"
+                            "   capacity\n"
+                            "0 1    1\n"
+                            "1 2    1\n"
+                            "2 3    1\n"
+                            "0 3    5\n"
+                            "0 3    10\n"
+                            "0 3    7\n"
+                            "4 2    1\n"
+                            "@attributes\n"
+                            "source 0\n"
+                            "target 3\n";
 
-void test_graph_read() {
+    char test_mtx_graph[] = "%%MatrixMarket matrix coordinate real general\n"
+                            "% (.mtx ids start from 0)\n"
+                            "% Sample graph in Matrix Market format\n"
+                            "5 5 7\n"
+                            "1 2 1\n"
+                            "2 3 1\n"
+                            "3 4 1\n"
+                            "1 4 5\n"
+                            "1 4 10\n"
+                            "1 4 7\n"
+                            "5 3 1\n";
 
-
-    char test_lgf_graph[] =
-        "@nodes\n"
-        "label\n"
-        "0\n"
-        "1\n"
-        "2\n"
-        "3\n"
-        "4\n"
-        "@arcs\n"
-        "   capacity\n"
-        "0 1    1\n"
-        "1 2    1\n"
-        "2 3    1\n"
-        "0 3    5\n"
-        "0 3    10\n"
-        "0 3    7\n"
-        "4 2    1\n"
-        "@attributes\n"
-        "source 0\n"
-        "target 3\n";
-
-    char test_mtx_graph[] =
-        "%%MatrixMarket matrix coordinate real general\n"
-		"% (.mtx ids start from 0)\n"
-		"% Sample graph in Matrix Market format\n"
-		"5 5 7\n"
-		"1 2 1\n"
-		"2 3 1\n"
-		"3 4 1\n"
-		"1 4 5\n"
-		"1 4 10\n"
-		"1 4 7\n"
-		"5 3 1\n";
-
-
-	ListGraph g_lgf, g_mtx;
-	ListGraph::EdgeMap<int> weights_lgf(g_lgf), weights_mtx(g_mtx);
+    ListGraph g_lgf, g_mtx;
+    ListGraph::EdgeMap<int> weights_lgf(g_lgf), weights_mtx(g_mtx);
 
     // Read lgf
-	std::istringstream input_lgf(test_lgf_graph);
-	GraphReader<ListGraph>(g_lgf, input_lgf).
-		edgeMap("capacity", weights_lgf).run();
+    std::istringstream input_lgf(test_lgf_graph);
+    GraphReader<ListGraph>(g_lgf, input_lgf)
+        .edgeMap("capacity", weights_lgf)
+        .run();
 
-	// Read mtx
-	std::istringstream input_mtx(test_mtx_graph);
-	readMtxGraph(g_mtx, weights_mtx, input_mtx);
+    // Read mtx
+    std::istringstream input_mtx(test_mtx_graph);
+    readMtxGraph(g_mtx, weights_mtx, input_mtx);
 
     // Todo: compare that the graphs match
-
 }
 
 int main()
--- src/mtx_reader.hpp
+++ src/mtx_reader.hpp
@@ -1,58 +1,68 @@
 #pragma once
 
-#include <vector>
 #include <iostream>
 #include <string>
-
+#include <vector>
 
 template <typename Graph, typename ArcMap>
-std::istream& readMtxGraph(Graph& graph, ArcMap& arc_map, std::istream& is = std::cin) {
-graph.clear();
+std::istream& readMtxGraph(
+    Graph& graph, ArcMap& arc_map, std::istream& is = std::cin)
+{
+    graph.clear();
 
-std::string line;
+    std::string line;
 
-// First line start with "%%MatrixMarket matrix coordinate*"
-if (!getline(is, line) || line.substr(0, 33) != "%%MatrixMarket matrix coordinate") {
-    std::cerr << "Invalid MatrixMarket header" << std::endl;
-    return is;
-}
-
-// Skip comments
-while (getline(is, line)) {
-    if (line[0] != '%') {
-    break;
-    }
-}
-
-// Read header
-int n, m;
-if (!(is >> n >> n >> m)) {
-    std::cerr << "Invalid MatrixMarket header" << std::endl;
-    return is;
-}
-
-// Add nodes
-std::vector<typename Graph::Node> nodes;
-for (int i = 0; i < n; ++i) {
-    nodes.push_back(graph.addNode());
-}
-
-// Add edges
-for (int i = 0; i < m; ++i) {
-	int u, v, w;
-    if (!(is >> u >> v)) {
+    // First line start with "%%MatrixMarket matrix coordinate*"
+    if (!getline(is, line) ||
+        line.substr(0, 33) != "%%MatrixMarket matrix coordinate")
+    {
+        std::cerr << "Invalid MatrixMarket header" << std::endl;
         return is;
     }
-    auto edge = graph.addEdge(nodes[u - 1], nodes[v - 1]);
-    if (is >> w) {
-		arc_map[edge] = w;
-    }
-    else {
-        // Default weight to 1
-		arc_map[edge] = 1;
-    }
-}
 
-return is;
-}
+    // Skip comments
+    while (getline(is, line))
+    {
+        if (line[0] != '%')
+        {
+            break;
+        }
+    }
 
+    // Read header
+    int n, m;
+    if (!(is >> n >> n >> m))
+    {
+        std::cerr << "Invalid MatrixMarket header" << std::endl;
+        return is;
+    }
+
+    // Add nodes
+    std::vector<typename Graph::Node> nodes;
+    for (int i = 0; i < n; ++i)
+    {
+        nodes.push_back(graph.addNode());
+    }
+
+    // Add edges
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v, w;
+        if (!(is >> u >> v))
+        {
+            return is;
+        }
+        auto edge = graph.addEdge(nodes[u - 1], nodes[v - 1]);
+        if (is >> w)
+        {
+            arc_map[edge] = w;
+        }
+        else
+        {
+            // Default weight to 1
+            arc_map[edge] = 1;
+        }
+    }
+
+    return is;
+}
